14f85eb3641cf22e8d33587e5a59a16d4f264c81
wdd_glb <1710031565@qq.com>
2019-08-11 17:38:00 +0800

    修改session bug

--- src/main/java/com/zero/system/interceptor/LogInterceptor.java
+++ src/main/java/com/zero/system/interceptor/LogInterceptor.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @Classname LogInterceptor
@@ -33,6 +34,10 @@ public class LogInterceptor implements HandlerInterceptor{
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String stringDate = DateUtil.getStringDate("yyyy-MM-dd HH:mm:ss");
         String username = null;
+        HttpSession session=request.getSession(false);
+        if(session==null){
+            return;
+        }
         if(request.getSession().getAttribute(Const.ADMIN) != null){
             Admin admin = (Admin)request.getSession().getAttribute(Const.ADMIN);
             username = admin.getUsername();
